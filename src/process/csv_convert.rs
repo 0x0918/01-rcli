946b91ca4ea2434f2e55142ac7cb95d24103e73c
0x0918 <yiyunbest@gmail.com>
2024-06-05 07:25:42 +0800

    features: add password

--- src/process/csv_convert.rs
+++ src/process/csv_convert.rs
@@ -21,7 +21,7 @@ struct Player {
 
 pub fn process_csv(input: &str, output: String, format: OutputFormat) -> anyhow::Result<()> {
     let mut reader = Reader::from_path(input)?;
-    let mut ret = Vec::with_capacity(128);
+    let mut ret: Vec<Value> = Vec::with_capacity(128);
     let headers = reader.headers()?.clone();
 
     for result in reader.records() {
