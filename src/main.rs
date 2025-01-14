b528bea3a06122c65e3902a6181140e117a501f3
0x0918 <yiyunbest@gmail.com>
2025-01-14 15:57:37 +0800

    feature: add cli support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, Rust!");
+use anyhow::Ok;
+use clap::Parser;
+use rcli::{process_csv, Opts};
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    match opts.cmd {
+        rcli::SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+    }
+    Ok(())
 }
